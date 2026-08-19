87389a49551adf5306e3f13f783bac49ec6c9d73
Darya Gayun <darya.mihelson@mail.ru>
2026-08-19 19:15:32 +0300

    all done

---
+++ helpers/user_generator.py
@@ -0,0 +1,14 @@
+﻿import random
+import string
+
+def generate_unique_email():
+    return f"user_{random.randint(10000, 99999)}@test.ru"
+
+def generate_password(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def generate_name(length=6):
+    return ''.join(random.choices(string.ascii_letters, k=length))
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
